a569821b3a056e3cbf9d5a83d73c9c6cf534a130
Sophie Wu <uwu@Sophies-MacBook-Pro.local>
2025-09-19 10:04:00 +0100

    kewl

---
+++ hi1.r
@@ -0,0 +1,8 @@
+print("hello world")           # tidyverse --> install.package("tidyverse")
+                               # library(tidyverse)            ^
+# assignment                   # do this in terminal/console - |
+a <- c(1,2,3,4,5,6) #i think its append???
+b <- rep(2,4) #REPLICATE FUNCTION so like 2 2 2 2 (four 2s)
+                                                                   #r markdown
+help(c) # this helps to understand what each functions do
+# tidyverse :: <- use functions.        package name :: function name
